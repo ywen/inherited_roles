2689736e212d977ffc40af7790b9f0af1462c1dd
Yi Wen <ywen.github@gmail.com>
2013-07-31 10:43:50 -0500

    Fix the build
    EqualByName needs to be required first

--- lib/inherited_roles.rb
+++ lib/inherited_roles.rb
@@ -1,4 +1,5 @@
 require "#{__dir__}/inherited_roles/model_initializer"
+require "#{__dir__}/inherited_roles/equal_by_name"
 Dir["#{File.dirname(__FILE__)}/inherited_roles/**/*.rb"].each do |f| 
   require f
 end
